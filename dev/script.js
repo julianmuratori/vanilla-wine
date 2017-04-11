492a95161f2564c2759f01a9d03b9af376f0eea8
Julian Muratori <julianmuratori@gmail.com>
2017-04-11 17:08:44 -0400

    more functionality

--- dev/script.js
+++ dev/script.js
@@ -1,37 +1,61 @@
 
 const wineApp = {};
+const categoryIds = []
 
 wineApp.apiKey = '2b72bf53d75160535f54fa07986c65dc';
 wineApp.endpoint = 'http://services.wine.com/api/beta2/service.svc/JSON/reference?';
 
 wineApp.init = function() {
-	wineApp.getSubject();
+	wineApp.grabInitialIds();
+	wineApp.getStyle();
 }
+
+// Stores initial wine type ids in an array so that if the user clicks "start over"
+// the page can be easily repopulated
+wineApp.grabInitialIds = () => {
+	$('.button').each((i, obj) => {
+		let eachId = ($(obj).attr('id'));
+		eachId = parseInt(eachId, 10);
+		categoryIds.push(eachId);
+	});
+};
 
 
 // Grabs the initial category selection from the user
-wineApp.getSubject = function() {
+wineApp.getStyle = () => {
 	$('.category').on('submit', (e) => {
 		e.preventDefault();
 		let selection = $('input:checked').val()
-		
+
 		if (selection === undefined) {
 			alert('Please select a category');
 		} else {
-			let category = wineApp.returnSubject(selection); 
+			console.log(selection)
+			let category = wineApp.returnStyle(selection);
 			$.when(category)
-			.then(function(data) {
-				data = data.Books;
-				console.log(data);
+				.then((results) => {
+					console.log(results);
+					wineApp.resultsCondense(results);
 			})
 		}
-		
 	})
 };
 
+// Takes grape names and associated ids and converts them to key-value pairs
+
+wineApp.resultsCondense = (results) => {
+	results = results.Books[1].Articles;
+	let resultsName = results.map((varietals) => varietals.Title);
+	let resultsId = results.map((varietals) => varietals.Id);
+	let resultPairs = {};
+	resultsName.forEach((key, idx) => resultPairs[key] = resultsId[idx]);
+
+	wineApp.grapeOptions(resultPairs);
+}
+
 // Makes an AJAX request for whichever category has been selected and adds introductory info on to the page
 
-wineApp.returnSubject = (selection) => $.ajax({
+wineApp.returnStyle = (selection) => $.ajax({
 	url: `${wineApp.endpoint}`,
 	method: 'GET',
 	dataType: 'json',
@@ -41,9 +65,68 @@ wineApp.returnSubject = (selection) => $.ajax({
 	}
 
 })
-// .then(function(data) {
-// 	data = data.Books;
-// 	console.log(data);
+
+
+// Appends grape varietals to the page and allows the user to make their choice
+
+wineApp.grapeOptions = (resultPairs) => {
+	wineApp.form();
+
+		for (let key in resultPairs) {
+			$("form").append(`<input type="radio" name="categories" id="${resultPairs[key]}">${key}`);
+		}
+
+		$("form").append(`<input type="submit" id="submitButton" class="button" value="Submit">`).append(`<input type="submit" id="startOver" class="button" value="Start Over">`);
+
+		wineApp.startOver();
+		wineApp.grapeChoice();
+}
+
+wineApp.grapeChoice = () => {
+	$("#submitButton").on("click", (e) => {
+		e.preventDefault();
+
+	})
+}
+
+// Controls the button to start from the beginning
+
+wineApp.startOver = () => {
+	$("#startOver").on("click", (e) => {
+		e.preventDefault();
+		wineApp.form();
+		let returns = [];
+		let queries = categoryIds.forEach((i) => {
+			const choice = wineApp.originalChoices(i)
+				$.when(choice)
+					.then((results) => {
+						// returns.push(results);
+						let names = results.Books[0].Articles[0].Title;
+						let ids = results.Books[0].Articles[0].Id;
+						$("form").prepend(`<input type="radio" name="categories" id="${ids}" value="${ids}">${names}`)
+					})
+		})
+			$("form").append(`<input type="submit" id="submitButton" class="button" value="Submit">`);
+			wineApp.getStyle();
+	});
+
+}
+
+// Makes AJAX call to repopulate original wine options
+wineApp.originalChoices = (i) => $.ajax({
+	url: `${wineApp.endpoint}`,
+	method: 'GET',
+	dataType: 'json',
+	data: {
+		filter: `categories(${i})`,
+		apikey: `${wineApp.apiKey}`
+	}
+});
+
+wineApp.form = () => {
+	$("form").remove();
+	$("main").append(`<form class="category">`);
+}
 
 
 $(wineApp.init);
--- public/script.js
+++ public/script.js
@@ -1,16 +1,28 @@
 'use strict';
 
 var wineApp = {};
+var categoryIds = [];
 
 wineApp.apiKey = '2b72bf53d75160535f54fa07986c65dc';
 wineApp.endpoint = 'http://services.wine.com/api/beta2/service.svc/JSON/reference?';
 
 wineApp.init = function () {
-	wineApp.getSubject();
+	wineApp.grabInitialIds();
+	wineApp.getStyle();
+};
+
+// Stores initial wine type ids in an array so that if the user clicks "start over"
+// the page can be easily repopulated
+wineApp.grabInitialIds = function () {
+	$('.button').each(function (i, obj) {
+		var eachId = $(obj).attr('id');
+		eachId = parseInt(eachId, 10);
+		categoryIds.push(eachId);
+	});
 };
 
 // Grabs the initial category selection from the user
-wineApp.getSubject = function () {
+wineApp.getStyle = function () {
 	$('.category').on('submit', function (e) {
 		e.preventDefault();
 		var selection = $('input:checked').val();
@@ -18,18 +30,37 @@ wineApp.getSubject = function () {
 		if (selection === undefined) {
 			alert('Please select a category');
 		} else {
-			var category = wineApp.returnSubject(selection);
-			$.when(category).then(function (data) {
-				data = data.Books;
-				console.log(data);
+			console.log(selection);
+			var category = wineApp.returnStyle(selection);
+			$.when(category).then(function (results) {
+				console.log(results);
+				wineApp.resultsCondense(results);
 			});
 		}
 	});
 };
 
+// Takes grape names and associated ids and converts them to key-value pairs
+
+wineApp.resultsCondense = function (results) {
+	results = results.Books[1].Articles;
+	var resultsName = results.map(function (varietals) {
+		return varietals.Title;
+	});
+	var resultsId = results.map(function (varietals) {
+		return varietals.Id;
+	});
+	var resultPairs = {};
+	resultsName.forEach(function (key, idx) {
+		return resultPairs[key] = resultsId[idx];
+	});
+
+	wineApp.grapeOptions(resultPairs);
+};
+
 // Makes an AJAX request for whichever category has been selected and adds introductory info on to the page
 
-wineApp.returnSubject = function (selection) {
+wineApp.returnStyle = function (selection) {
 	return $.ajax({
 		url: '' + wineApp.endpoint,
 		method: 'GET',
@@ -41,9 +72,65 @@ wineApp.returnSubject = function (selection) {
 
 	});
 };
-// .then(function(data) {
-// 	data = data.Books;
-// 	console.log(data);
 
+// Appends grape varietals to the page and allows the user to make their choice
+
+wineApp.grapeOptions = function (resultPairs) {
+	wineApp.form();
+
+	for (var key in resultPairs) {
+		$("form").append('<input type="radio" name="categories" id="' + resultPairs[key] + '">' + key);
+	}
+
+	$("form").append('<input type="submit" id="submitButton" class="button" value="Submit">').append('<input type="submit" id="startOver" class="button" value="Start Over">');
+
+	wineApp.startOver();
+	wineApp.grapeChoice();
+};
+
+wineApp.grapeChoice = function () {
+	$("#submitButton").on("click", function (e) {
+		e.preventDefault();
+	});
+};
+
+// Controls the button to start from the beginning
+
+wineApp.startOver = function () {
+	$("#startOver").on("click", function (e) {
+		e.preventDefault();
+		wineApp.form();
+		var returns = [];
+		var queries = categoryIds.forEach(function (i) {
+			var choice = wineApp.originalChoices(i);
+			$.when(choice).then(function (results) {
+				// returns.push(results);
+				var names = results.Books[0].Articles[0].Title;
+				var ids = results.Books[0].Articles[0].Id;
+				$("form").prepend('<input type="radio" name="categories" id="' + ids + '" value="' + ids + '">' + names);
+			});
+		});
+		$("form").append('<input type="submit" id="submitButton" class="button" value="Submit">');
+		wineApp.getStyle();
+	});
+};
+
+// Makes AJAX call to repopulate original wine options
+wineApp.originalChoices = function (i) {
+	return $.ajax({
+		url: '' + wineApp.endpoint,
+		method: 'GET',
+		dataType: 'json',
+		data: {
+			filter: 'categories(' + i + ')',
+			apikey: '' + wineApp.apiKey
+		}
+	});
+};
+
+wineApp.form = function () {
+	$("form").remove();
+	$("main").append('<form class="category">');
+};
 
 $(wineApp.init);
